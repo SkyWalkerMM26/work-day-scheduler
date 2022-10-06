7193547f144eaebf8149cf142a64075d9a1aa588
SkyWalkerMM26 <myia_moua26@yahoo.com>
2022-10-06 07:39:32 -0700

    Give html more timeblock from 9am to 5pm and identify each row by an id

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,10 @@
 //Pseudocode
-//When the user opens the page, there should be a schedule for the user to enter their event information.
-//Insert a jQuery UI to install a schedule.
+// When the user opens the page, there should be the current date
+// use moment().format(the day of the week and date)
+// There should be time blocks from 9AM to 5PM for the user to input and save.
+// use bootstrap to assist with the input and save button. 
+// The time block should be color coded of what is considered the past, present, and future. 
+// Save the user input into the local storage: Hint hint(window.onbeforeunload = function(){localStorage.setItem.("event", $("#id").val());})
+
+var headerTime = moment().format("dddd, MMMM Do");
+$("#currentDay").text(headerTime);

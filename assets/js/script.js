ebdbfe702ce6a83c5d7f694db970e005e7e57ae1
SkyWalkerMM26 <myia_moua26@yahoo.com>
2022-10-06 08:55:29 -0700

    compare current hour to set id= to do if statements to add class and remove class

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,5 +6,29 @@
 // The time block should be color coded of what is considered the past, present, and future. 
 // Save the user input into the local storage: Hint hint(window.onbeforeunload = function(){localStorage.setItem.("event", $("#id").val());})
 
-var headerTime = moment().format("dddd, MMMM Do");
-$("#currentDay").text(headerTime);
+$(document).ready(function (){
+    var headerTime = moment().format("dddd, MMMM Do");
+    $("#currentDay").text(headerTime);
+
+    var currentHH = moment().format("H");
+
+    $(".row").each(function(){
+        var inputTime = $(this).attr("id");
+
+        if (currentHH == inputTime) {
+            $(this).addClass("present");
+            $(this).children("description").addClass("white-text");
+        } else if (currentHH < inputTime){
+            $(this).removeClass("present");
+            $(this).addClass("future");
+        } else if (currentHH > inputTime){
+            $(this).removeClass("future");
+            $(this).addClass("past");
+        }
+    });
+
+    
+    
+})
+
+
